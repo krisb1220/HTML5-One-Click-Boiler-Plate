4c5c8620602dadfe225e6a48c55f9fe10058d4ed
Kris Baillargeon <krisb1220@gmail.com>
2018-02-03 06:12:55 -0500

    Create basic-window-bp.bat

---
+++ basic-window-bp.bat
@@ -0,0 +1,22 @@
+mkdir styles 
+mkdir js 
+mkdir src 
+
+echo html > index.html 
+echo php > index.php
+
+
+cd src
+
+cd styles 
+
+echo scss > master.css
+echo scss > master.scss
+echo sass > master.sass
+echo less > master.less
+
+cd ../
+cd js 
+
+echo javascript > master.js 
+echo json > master.json
